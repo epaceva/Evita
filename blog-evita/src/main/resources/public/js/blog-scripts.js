ef0dcf8de4b0b9663ae27a435d132c890c9a6aaa
Evelina Paceva <paceva.eva@gmail.com>
2016-08-31 17:38:14 +0300

    .

--- blog-evita/src/main/resources/public/js/blog-scripts.js
+++ blog-evita/src/main/resources/public/js/blog-scripts.js
@@ -5,4 +5,10 @@ $(function(){
 	setTimeout(function(){
 		$('#messages li.info').fadeOut();
 	}, 3000);
+	
+	$(".nav a").on("click", function(){
+		   $(".nav").find(".active").removeClass("active");
+		   $(this).parent().addClass("active");
+		});
 });
+
